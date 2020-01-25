f3619b7f56382625e4e553b0a5e58080a64fee9b
Joris de Vrede <jorisdevrede@gmail.com>
2020-01-25 23:48:54 +0100

    restructured metrics

--- one_exporter.go
+++ one_exporter.go
@@ -34,31 +34,67 @@ type config struct {
 }
 
 var (
-	poolTotalMemGauge = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "one_pool_totalmem",
-		Help: "total memory of all hosts in opennebula",
-	})
-	poolUsedMemGauge = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "one_pool_usedmem",
-		Help: "used memory in all hosts in opennebula",
-	})
-	poolTotalCPUGauge = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "one_pool_totalcpu",
-		Help: "total cpu of all hosts in opennebula",
-	})
-	poolUsedCPUGauge = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "one_pool_usedcpu",
-		Help: "used cpu in all hosts in opennebula",
-	})
-	poolActiveHostsGauge = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "one_pool_activehosts",
-		Help: "number of active hosts in opennebula",
-	})
-	poolRunningVMsGauge = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "one_pool_runningvms",
-		Help: "number of running virtual machines in opennebula",
-	})
+	clusterMetrics = make(map[string]*prometheus.GaugeVec)
+	hostMetrics    = make(map[string]*prometheus.GaugeVec)
 )
+
+func initCollectors() {
+	clusterMetrics["TotalMem"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_cluster_totalmem",
+			Help: "total memory available in cluster",
+		},[]string{"cluster"})
+
+	clusterMetrics["UsedMem"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_cluster_usedmem",
+			Help: "real used memory in cluster",
+		},[]string{"cluster"})
+
+	clusterMetrics["TotalCPU"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_cluster_totalcpu",
+			Help: "total cpu available in cluster",
+		},[]string{"cluster"})
+
+	clusterMetrics["UsedCPU"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_cluster_usedcpu",
+			Help: "real used cpu in cluster",
+		},[]string{"cluster"})
+
+	clusterMetrics["RunningVMs"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_cluster_runningvms",
+			Help: "running virtual machines in cluster",
+		},[]string{"cluster"})
+
+	clusterMetrics["ActiveHosts"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_cluster_activehosts",
+			Help: "succesfully monitored hosts in cluster",
+		},[]string{"cluster"})
+
+	hostMetrics["TotalMem"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_host_totalmem",
+			Help: "total memory available on host",
+		},[]string{"cluster", "host"})
+
+	hostMetrics["UsedMem"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_host_usedmem",
+			Help: "real used memory on host",
+		},[]string{"cluster", "host"})
+
+	hostMetrics["TotalCPU"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_host_totalcpu",
+			Help: "total cpu available on host",
+		},[]string{"cluster", "host"})
+
+	hostMetrics["UsedCPU"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_host_usedcpu",
+			Help: "real used cpu on host",
+		},[]string{"cluster", "host"})
+
+	hostMetrics["RunningVMs"] = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "one_host_runningvms",
+			Help: "running virtual machines on host",
+		},[]string{"cluster", "host"})
+
+}
 
 // recordMetrics from OpenNebula
 func recordMetrics(config config, logger log.Logger) {
@@ -76,13 +112,10 @@ func recordMetrics(config config, logger log.Logger) {
 			panic(err)
 		}
 
-		var totalMem int = 0
-		var usedMem int = 0
-		var totalCPU int = 0
-		var usedCPU int = 0
-		var runningVMs int = 0
-
-		var activeHosts int = 0
+		type metrics struct {
+			cluster, metric string
+		}
+		sum := make(map[metrics]int)
 
 		for _, host := range pool.Hosts {
 
@@ -94,24 +127,29 @@ func recordMetrics(config config, logger log.Logger) {
 				"UsedCPU", host.Share.UsedCPU,
 				"RunningVMs", host.Share.RunningVMs)
 
-			totalMem = totalMem + host.Share.TotalMem
-			usedMem = usedMem + host.Share.UsedMem
-			totalCPU = totalCPU + host.Share.TotalCPU
-			usedCPU = usedCPU + host.Share.UsedCPU
-			runningVMs = runningVMs + host.Share.RunningVMs
+			// record host metrics
+			hostMetrics["TotalMem"].With(prometheus.Labels{"cluster": host.Cluster, "host": host.Name}).Set(float64(host.Share.TotalMem))
+			hostMetrics["UsedMem"].With(prometheus.Labels{"cluster": host.Cluster, "host": host.Name}).Set(float64(host.Share.UsedMem))
+			hostMetrics["TotalCPU"].With(prometheus.Labels{"cluster": host.Cluster, "host": host.Name}).Set(float64(host.Share.TotalCPU))
+			hostMetrics["UsedMem"].With(prometheus.Labels{"cluster": host.Cluster, "host": host.Name}).Set(float64(host.Share.UsedMem))
+			hostMetrics["RunningVMs"].With(prometheus.Labels{"cluster": host.Cluster, "host": host.Name}).Set(float64(host.Share.RunningVMs))
+
+			// sum cluster metrics
+			sum[metrics{host.Cluster, "TotalMem"}] = sum[metrics{host.Cluster, "TotalMem"}] + host.Share.TotalMem
+			sum[metrics{host.Cluster, "UsedMem"}] = sum[metrics{host.Cluster, "UsedMem"}] + host.Share.UsedMem
+			sum[metrics{host.Cluster, "TotalCPU"}] = sum[metrics{host.Cluster, "TotalCPU"}] + host.Share.TotalCPU
+			sum[metrics{host.Cluster, "UsedCPU"}] = sum[metrics{host.Cluster, "UsedCPU"}] + host.Share.UsedCPU
+			sum[metrics{host.Cluster, "RunningVMs"}] = sum[metrics{host.Cluster, "RunningVMs"}] + host.Share.RunningVMs
 
 			if host.StateRaw == 2 {
-				activeHosts = activeHosts + 1
+				sum[metrics{host.Cluster, "ActiveHosts"}] = sum[metrics{host.Cluster, "ActiveHosts"}] + 1
 			}
 		}
 
-		poolTotalMemGauge.Set(float64(totalMem))
-		poolUsedMemGauge.Set(float64(usedMem))
-		poolTotalCPUGauge.Set(float64(totalCPU))
-		poolUsedCPUGauge.Set(float64(usedCPU))
-		poolRunningVMsGauge.Set(float64(runningVMs))
-
-		poolActiveHostsGauge.Set(float64(activeHosts))
+		for key, value := range sum {
+			// record cluster metrics
+			clusterMetrics[key.metric].With(prometheus.Labels{"cluster": key.cluster}).Set(float64(value))
+		}
 
 		time.Sleep(time.Duration(config.interval) * time.Second)
 	}
@@ -180,6 +218,8 @@ func main() {
 
 	config := newConfig(*cfgFile, logger)
 	level.Debug(logger).Log("msg", "loaded config", "user", config.user, "endpoint", config.endpoint)
+
+	initCollectors()
 
 	go recordMetrics(config, logger)
 
